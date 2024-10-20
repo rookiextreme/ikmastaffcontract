9ec8b1a6b1a636505609403003f6fc2f4af1c537
rookiextreme <meor.nazrin1994@gmail.com>
2024-10-20 16:39:20 +0800

    bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RedirectIfAuthenticated::redirectUsing(function () {
+            $user = Auth::user();
+            if($user->hasRole('super-admin|admin')){
+                return route('admin.user.list', absolute: false);
+            }else if($user->hasRole(['staff'])){
+                return route('staff.profile', ['user_id' => $user->id, 'page' => 'main'],  absolute: false);
+            }
+        });
+
+        if(app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
