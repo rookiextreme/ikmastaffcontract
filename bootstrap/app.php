70f168de724e6d864592366d22144dab68b1210b
rookiextreme <meor.nazrin1994@gmail.com>
2024-10-27 13:59:17 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'meta-test' // <-- exclude this route
+            'meta-test-verify' // <-- exclude this route
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
