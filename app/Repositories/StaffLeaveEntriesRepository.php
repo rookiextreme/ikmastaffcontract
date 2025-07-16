df16c707828c79d2deefcb158f85c0be29f16e64
rookiextreme <90501419+rookiextreme@users.noreply.github.com>
2025-07-17 03:21:33 +0800

    Update StaffLeaveEntriesRepository.php

--- app/Repositories/StaffLeaveEntriesRepository.php
+++ app/Repositories/StaffLeaveEntriesRepository.php
@@ -147,12 +147,12 @@ class StaffLeaveEntriesRepository
 
                 $sLeave = $staffPosition->getStaffLeave;
                 if($getLeaveCategory->is_mc == true){
-                    $sLeave->mc_taken = $hTaken;
-                    $sLeave->mc_balance = $sLeave->mc_balance + $hTaken;
+                    $sLeave->mc_taken = $sLeave->mc_taken + $hTaken;
+                    $sLeave->mc_balance = $sLeave->mc_balance - $hTaken;
                 }elseif($getLeaveCategory->is_half_day == true){
 
                 }else{
-                    $sLeave->leave_taken = $hTaken;
+                    $sLeave->leave_taken = $sLeave->leave_taken + $hTaken;
                     $sLeave->leave_balance = $sLeave->leave_balance - $hTaken;
                 }
 
